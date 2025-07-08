4651be39bdc5bc0a53fd6c3532bec084ffacbd17
Lee554-svg <mumyoung1144@gmail.com>
2025-07-08 12:26:12 +0900

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,24 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  name: String,
+  text: String,
+  safeText: String
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  safeTitle: String,
+  content: String,
+  safeContent: String,
+  author: String,
+  createdAt: String,
+  imageUrl: String,
+  comments: [commentSchema],
+  upvotes: Number,
+  downvotes: Number,
+  views: Number
+});
+
+module.exports = mongoose.model('Post', postSchema);
 
